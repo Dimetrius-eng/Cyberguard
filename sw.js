692d8c9937dbebf07cf381bf5c4d7c5985758670
Dimetrius-eng <mydimka08@gmail.com>
2025-12-03 15:18:19 +0200

    sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'cyberguard-v39'; 
+const CACHE_NAME = 'cyberguard-v40'; 
 const ASSETS_TO_CACHE = [
     './',
     './index.html',
@@ -48,6 +48,7 @@ self.addEventListener('fetch', (evt) => {
     );
 
 });
+
 
 
 
